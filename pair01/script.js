113f94c0233b3a5f30a643c9faa6b72ebabf6ad1
Nazar <142441304+Nazaza-prog@users.noreply.github.com>
2026-09-07 11:15:17 +0300

    Add files via upload

---
+++ pair01/script.js
@@ -0,0 +1,83 @@
+//42
+//4.5
+
+// 'use Str'
+
+//true;
+// 1
+
+
+//false;
+// 0;
+// NaN;
+// null
+// undefined
+
+// x = y + 10
+
+// var a;
+// a = 10
+//
+// console.log(a);
+// var b = 20;
+// var c = 0, d = 12;
+//
+// let aNew = '32';
+
+// a = 27
+// let a;
+// console.log(a);
+
+// const a = 4;
+
+
+// let a = 15, b =26
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log(a / b);
+// console.log(a % b);
+//
+// a += 1;
+// a++;
+// a--;
+// ++a;
+// let a = 2;
+// b = a++ суфіксна
+// b = ++a префіксна
+// console.log(b);
+
+// let a = '4';
+// let b = 2;
+//
+// console.log(a - b);
+// console.log(a + b);
+// console.log((a - 0) + b);
+//
+//
+// a = Number(a);
+// console.log(a + b);
+
+
+// Інтерполяція - формат рядкового значення (f'строка')
+
+// let name = "Ivan";
+// let age = 50;
+//
+// console.log("Привіт" + name + "!" + "тобі" + age + "років");
+// console.log('Привіт ${name}! тобі ${age} років');
+
+// console.info("Hello, World!");
+// console.log("Hello, World!");
+// console.warn("Hello, World!");
+// console.error("Hello, World!");
+
+// alert("hello")
+// let name = prompt("Як тебе звати?");
+// alert(name);
+
+let a = Number(prompt('number1'));
+let b = +prompt('number2');
+console.log(a + b);
+
